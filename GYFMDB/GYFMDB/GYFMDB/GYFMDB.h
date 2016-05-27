de231da0ee8ec66cd6142b96f2bc9bd0d0cf3175
jianglc <jianglc@gyist.com>
2016-05-27 18:29:38 +0800

    add method

--- GYFMDB/GYFMDB/GYFMDB/GYFMDB.h
+++ GYFMDB/GYFMDB/GYFMDB/GYFMDB.h
@@ -8,6 +8,45 @@
 
 #import <Foundation/Foundation.h>
 
+@interface GYKeyValueItem : NSObject
+
+@property (strong, nonatomic) NSString *key;
+@property (strong, nonatomic) id object;
+@property (strong, nonatomic) NSDate *createdTime;
+
+
+@end
+
 @interface GYFMDB : NSObject
+
+-(instancetype)sharedDB;
+
+- (id)initWithDBWithPath:(NSString *)dbPath;
+
+- (void)createTableWithName:(NSString *)tableName;
+
+- (BOOL)isTableExists:(NSString *)tableName;
+
+- (BOOL)clearTable:(NSString *)tableName;
+
+/*** 以下为用keyValue形式存储数据库方法****/
+
+- (BOOL)saveObject:(id)object withKey:(NSString *)objectKey intoTable:(NSString *)tableName;
+
+- (id)getObjectByKey:(NSString *)objectKey fromTable:(NSString *)tableName;
+
+- (GYKeyValueItem *)getKeyValueItemByKey:(NSString *)objectKey fromTable:(NSString *)tableName;
+
+- (NSArray *)getAllKeyValueItemsFromTable:(NSString *)tableName;
+
+- (NSUInteger)getCountFromTable:(NSString *)tableName;
+
+- (BOOL)deleteObjectByKey:(NSString *)objectKey fromTable:(NSString *)tableName;
+
+- (BOOL)deleteObjectsByKeyArray:(NSArray *)objectKeyArray fromTable:(NSString *)tableName;
+
+/***********************************/
+
+
 
 @end
--- GYFMDB/GYFMDB/GYFMDB/GYFMDB.m
+++ GYFMDB/GYFMDB/GYFMDB/GYFMDB.m
@@ -8,6 +8,75 @@
 
 #import "GYFMDB.h"
 
+#import "FMDatabase.h"
+#import "FMDatabaseQueue.h"
+
+#ifdef DEBUG
+#define debugLog(...)    NSLog(__VA_ARGS__)
+#define debugMethod()    NSLog(@"%s", __func__)
+#define debugError()     NSLog(@"Error at %s Line:%d", __func__, __LINE__)
+#else
+#define debugLog(...)
+#define debugMethod()
+#define debugError()
+#endif
+
+
+#define PATH_DOCUMENT    [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0]
+
+#define PATH_CACHE    [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0]
+
+@interface GYFMDB()
+
+@property (strong, nonatomic) FMDatabaseQueue * dbQueue;
+
+@end
+
 @implementation GYFMDB
+
+- (id)initDBWithName:(NSString *)dbName {
+    self = [super init];
+    if (self) {
+        NSString * dbPath = [PATH_DOCUMENT stringByAppendingPathComponent:dbName];
+        debugLog(@"dbPath = %@", dbPath);
+        if (_dbQueue) {
+            [self close];
+        }
+        _dbQueue = [FMDatabaseQueue databaseQueueWithPath:dbPath];
+    }
+    return self;
+}
+
+- (id)initWithDBWithPath:(NSString *)dbPath {
+    self = [super init];
+    if (self) {
+        debugLog(@"dbPath = %@", dbPath);
+        if (_dbQueue) {
+           
+        }
+        _dbQueue = [FMDatabaseQueue databaseQueueWithPath:dbPath];
+    }
+    return self;
+}
+
++ (instancetype)sharedInstance
+{
+    static dispatch_once_t onceToken;
+    static GYFMDB *_singleton = nil;
+   
+    dispatch_once(&onceToken,^{
+        _singleton = [[GYFMDB alloc]init];
+    });
+    return _singleton;
+}
+
+-(BOOL)createDataBaseWithPath:(NSString*)dbPath{
+
+    return YES;
+}
+- (void)close {
+    [_dbQueue close];
+    _dbQueue = nil;
+}
 
 @end
